ed88c7e666257a39ba1689361bbb9d9b7e765ea7
Bruno Marazzi <brunomarazzidev@gmail.com>
2022-10-03 15:01:57 -0300

    adding exception handling

--- cSharpCalculator/Program.cs
+++ cSharpCalculator/Program.cs
@@ -14,23 +14,34 @@
             {
                 Console.Clear();
 
-                Console.WriteLine("-------------------------");
-                Console.WriteLine("Simple Calculator Program");
-                Console.WriteLine("-------------------------");
+                Console.WriteLine("\t\t\t\t-------------------------");
+                Console.WriteLine("\t\t\t\tSimple Calculator Program");
+                Console.WriteLine("\t\t\t\t-------------------------");
 
+                bool valid = false;
                 Console.Write("Insert the first number: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
+                valid = double.TryParse(Console.ReadLine(), out num1);
+
+                while (!valid)
+                {
+                    Console.Write("Please insert a valid number: ");
+                    valid = double.TryParse(Console.ReadLine(), out num1);
+                }
+
+                // https://stackoverflow.com/questions/18671774/c-sharp-allow-user-to-input-again-until-correct
+
+                Console.WriteLine("\tChoose an operation: ");
+                Console.WriteLine("\t+ : Addition");
+                Console.WriteLine("\t- : Subtraction");
+                Console.WriteLine("\t* : Multiplication");
+                Console.WriteLine("\t/ : Division");
+                string operation = Console.ReadLine();
+                // validar aqui se a opção foi válida              
 
                 Console.Write("Insert the second number: ");
                 num2 = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("Choose an operation: ");
-                Console.WriteLine("+ : Addition");
-                Console.WriteLine("- : Subtraction");
-                Console.WriteLine("* : Multiplication");
-                Console.WriteLine("/ : Division");
-
-                switch (Console.ReadLine())
+                switch (operation)
                 {
                     case "+":
                         result = num1 + num2;
@@ -56,7 +67,7 @@
             } while (Console.ReadLine().ToUpper() == "Y");
  
 
-            Console.WriteLine("Bye! Press ENTER twice to exit");
+            Console.WriteLine("Press ENTER twice to exit");
             Console.ReadKey();
         }
     }
